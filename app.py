d8204ee9e0fa372cb3437d5045b123a8dbba9993
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-03-08 13:07:57 +0530

    Almost done

--- app.py
+++ app.py
@@ -9,18 +9,21 @@ import requests
 
 
 pipe = joblib.load('Pipeline_new.plk')
+enc = joblib.load('Label_encoder.plk')
+
 
 apikey='04b51110e62d2ee68a1d58b2834fc9ed'
 url = f"https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=in&max=20&apikey={apikey}"
 response = requests.get(url)
-data = response.json()
-
 
 response = requests.get(url)
 
 data = pd.DataFrame(response.json()['articles'])
+data = data.rename(columns={'title' : 'headlines'})
 
-st.dataframe(data)
+data['category'] = enc.inverse_transform(pipe.predict(data[['headlines', 'description', 'content']]))
+
+st.dataframe(data[['headlines', 'description', 'content', 'url', 'image', 'category']])
 
 
 
